c9d4ef53813169a3ce882d10868042b78b9645a3
Zino <2277206@cegepmontpetit.ca>
2025-01-30 14:26:28 -0500

    Exercice Relation (PAS FINI (truc de badges))

--- Models/Platform.cs
+++ Models/Platform.cs
@@ -2,12 +2,13 @@
 
 namespace RelationsNaN.Models
 {
-    public class Genre
+    public class Platform
     {
         public int Id { get; set; }
         public string Name { get; set; }
 
         [ValidateNever]
         public List<Game> Games { get; set; }
+
     }
 }
